fa061c833973bb8c353163838d26c17a6d66088e
abulhussian12 <abulhussian1229@gmail.com>
2024-10-06 10:37:32 +0530

    Intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brandyRed: '#854632',
+        eggShell: '#F3E5D7',
+        wengeBrown: '#5F564D',
+        darkRasberry: '#7A284E',
+        whiteCoffe:'#FFF7FB',
+        darkCharcoal:'#312E2C',
+        
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
